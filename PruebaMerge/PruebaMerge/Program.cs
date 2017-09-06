7522500ae6e0f96bd1f9bcbd1e8af839555bd937
vgarciahoy <vgarciahoyas@deloitte.es>
2017-09-06 12:21:58 +0200

    8 - Cambio en desarrollo despues de haber hecho el merge con desarrollo auxiliar

--- PruebaMerge/PruebaMerge/Program.cs
+++ PruebaMerge/PruebaMerge/Program.cs
@@ -13,6 +13,8 @@ namespace PruebaMerge
 
             //1 - Modificacion fichero en desarrollo 12:02
             //4 - Modificacion fichero en desarrollo auxiliar 12:10
+            // 8 - Cambio en desarrollo desdepues de haber hecho el merge con desarrollo auxiliar
+
         }
     }
 }
