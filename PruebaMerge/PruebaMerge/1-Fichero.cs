81a98d8bedc03ea1edcb63dd0e6d60d87febf458
vgarciahoy <vgarciahoyas@deloitte.es>
2017-09-06 12:32:58 +0200

    //12 - Cambio en desarrollo 12:32

--- PruebaMerge/PruebaMerge/1-Fichero.cs
+++ PruebaMerge/PruebaMerge/1-Fichero.cs
@@ -10,5 +10,7 @@ namespace PruebaMerge
     {
         //4 - Modificacion fichero en desarrollo auxiliar 12:10
         // 8 - Cambio en desarrollo desdepues de haber hecho el merge con desarrollo auxiliar
+        //12 - Cambio en desarrollo 12:32
+
     }
 }
